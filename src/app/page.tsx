f1c673389276604969bde9003650243c128ba589
Ryan Goh <ryangohce@gmail.com>
2024-01-02 15:06:58 +0800

    implemented functioning wallet adapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+"use client"
 import { UnifiedWalletProvider, UnifiedWalletButton } from "@jup-ag/wallet-adapter";
+import { IWalletNotification } from "@jup-ag/wallet-adapter/dist/types/contexts/WalletConnectionProvider";
 
 const App = () => {
+  const WalletNotification = {
+    onConnect: (props: IWalletNotification) => {
+      // handle connect notification
+      console.log('Received wallet notification:', props);
+      return null;
+    },
+    onConnecting: (props: IWalletNotification) => {
+      // handle connecting notification
+      console.log('Received wallet notification:', props);
+      return null;
+    },
+    onDisconnect: (props: IWalletNotification) => {
+      // handle disconnect notification
+      console.log('Received wallet notification:', props);
+      return null;
+    },
+    onNotInstalled: (props: IWalletNotification) => {
+      // handle not installed notification
+      console.log('Received wallet notification:', props);
+      return null;
+    },
+  };
+
   return (
     <UnifiedWalletProvider
       wallets={[]}
@@ -13,7 +38,7 @@ const App = () => {
           url: "https://jup.ag",
           iconUrls: ["https://jup.ag/favicon.ico"],
         },
-        notificationCallback: {},
+        notificationCallback: WalletNotification,
         walletlistExplanation: {
           href: "https://station.jup.ag/docs/additional-topics/wallet-list",
         },
